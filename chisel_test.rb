e15743f7d3a25481c128f8a372b1877631c03963
MLee21 <minnie.lee89@gmail.com>
2015-02-19 15:24:58 -0700

    revised tests to include parameters on methods. tests passing.

--- chisel_test.rb
+++ chisel_test.rb
@@ -16,19 +16,14 @@ class ChiselTest < MiniTest::Test
 
 	def test_header_1_is_substituted
 		chisel = Chisel.new
-		document = '#My Life in Desserts'
-		assert_equal '<h1>My Life in Desserts</h1>', chisel.header_1
+		document = '#This is my text.'
+		assert_equal '<h1>This is my text.</h1>', chisel.header_1(document)
 	end	
-
-	def it_has_a_header_2
-		chisel = Chisel.new
-		assert_equal '##Chapter 1: The Beginning', chisel.header_2
-	end
 
 	def test_header_2_is_substituted
 		chisel = Chisel.new
-		document_part_two = '##Chapter 1: The Beginning'
-		assert_equal '<h2>Chapter 1: The Beginning</h2>',chisel.header_2
+		document = '##Chapter 1: The Beginning'
+		assert_equal '<h2>Chapter 1: The Beginning</h2>',chisel.header_2(document)
 	end
 
 	def test_paragraph_has_html_tags
