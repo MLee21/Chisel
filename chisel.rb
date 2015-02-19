9dad6bbc59933c037fda39e53cedffa2289ea68b
MLee21 <minnie.lee89@gmail.com>
2015-02-19 14:51:33 -0700

    fixed asterisks method. modified paragraph method. Working on refactoring.

--- chisel.rb
+++ chisel.rb
@@ -1,45 +1,74 @@
-
 class Chisel
 
 	attr_reader :paragraph, :document, :document_part_two
 	# 1. WE ARE SUBSTITUTING "#" WITH <h1> USING GSUB #
 	# 2. FIND A WAY TO RUN THROUGH STRING TO END AND ADD </h1>
 	def initialize
-		@paragraph = '"You just *have* to try the cheesecake," he said. "Ever since it appeared in
- 		**Food & Wine** this place has been packed every night."'
- 		@document = '#My Life in Desserts'
- 		@document_part_two = '##Chapter 1: The Beginning'
-	end	
+    @paragraph = '"You just *have* to try the cheesecake," he said. "Ever since it appeared in
+    **Food & Wine** this place has been packed every night."'
+    @document = '#My Life in Desserts'
+    @document_part_two = '##Chapter 1: The Beginning'
+  end	
 
 
-	def header_1
-		substitute_h1 = @document.gsub("#","<h1>")
-		substitute_h1 + "</h1>"
-	end
-		
-	def header_2
-    	substitute_h2 = @document_part_two.gsub("##","<h2>")
-    	substitute_h2 + "</h2>"
-	end
+  def header_1
+    substitute_h1 = @document.gsub("#","<h1>")
+    substitute_h1 + "</h1>"
+  end
 
-	def paragraph
-		"<p>" + "#{@paragraph}" + "</p>"
-	end
+  def header_2
+    substitute_h2 = @document_part_two.gsub("##","<h2>")
+    substitute_h2 + "</h2>"
+  end
+
+  def paragraph(text)
+    "<p>#{text}</p>"
+  end
+
+  def replace_wrapper_tag(text, md_marker, open_tag, close_tag)
+    in_a_tag = false
+    while text.include?(md_marker)
+      if !in_a_tag
+        text.sub!(md_marker,open_tag)
+        in_a_tag = true
+      else
+        text.sub!(md_marker,close_tag)
+        in_a_tag = false
+      end
+    end
+    text
+  end
+
+  def single_asterisks(text)
+    replace_wrapper_tag(text, "*", "<em>", "</em>")
+  end
+
+  def double_asterisks(text)
+    replace_wrapper_tag(text, "**", "<strong>", "</strong>")
+  end
 
 	#Run through sentence and find * encasing a word on each side 
 	#and **	encasing a word on each side
-	def asterisks
-		asterisks = @paragraph.split.map do |word| 
-			if word.include?("*")
-				word.gsub("*","<em>")
-			elsif word.include?("**")	
-				word.gsub("**","<strong>")
-			else	
-				word
-			end
-		end
-		asterisks.join(" ")
-	end
+	def asterisks(text)
+    asterisks = text.split.map do |word|
+      if word.include?("**")
+        word.sub!("**","<strong>")
+        word.sub!("**","</strong>")
+        word
+      elsif word.include?("*")
+        word.sub!("*","<em>")
+        word.sub!("*","</em>")
+        word
+      else	
+        word
+      end
+    end 
+    asterisks.join(" ")
+  end
+
+	# def replace_front_asterisks
+	# 	gsub("**","<strong>")
+	# end
 
 # # parser = Chisel.new
 # # output = parser.parse(document)
@@ -51,10 +80,6 @@ class Chisel
 end
 
 chisel = Chisel.new
-print chisel.asterisks
-
-# <p>
-#   "You just <em>have</em> to try the cheesecake," he said. "Ever since it appeared in
-#   <strong>Food &amp; Wine</strong> this place has been packed every night."
-# </p>
+print chisel.asterisks('"You just *have* to try the cheesecake," he said. "Ever **since** it appeared in
+    **Food & Wine** this place has been packed every night."')
 
