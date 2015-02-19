68f5c515191a0f6851d924402b40817cc82f8c34
MLee21 <minnie.lee89@gmail.com>
2015-02-19 15:24:39 -0700

    revised to include parameters. tests passing.

--- chisel.rb
+++ chisel.rb
@@ -1,23 +1,15 @@
 class Chisel
 
-	attr_reader :paragraph, :document, :document_part_two
 	# 1. WE ARE SUBSTITUTING "#" WITH <h1> USING GSUB #
 	# 2. FIND A WAY TO RUN THROUGH STRING TO END AND ADD </h1>
-	def initialize
-    @paragraph = '"You just *have* to try the cheesecake," he said. "Ever since it appeared in
-    **Food & Wine** this place has been packed every night."'
-    @document = '#My Life in Desserts'
-    @document_part_two = '##Chapter 1: The Beginning'
-  end	
 
-
-  def header_1
-    substitute_h1 = @document.gsub("#","<h1>")
+  def header_1(text)
+    substitute_h1 = text.gsub("#","<h1>")
     substitute_h1 + "</h1>"
   end
 
-  def header_2
-    substitute_h2 = @document_part_two.gsub("##","<h2>")
+  def header_2(text)
+    substitute_h2 = text.gsub("##","<h2>")
     substitute_h2 + "</h2>"
   end
 
@@ -79,7 +71,5 @@ class Chisel
 ##.insert for h1, h2, and p
 end
 
-chisel = Chisel.new
-print chisel.asterisks('"You just *have* to try the cheesecake," he said. "Ever **since** it appeared in
-    **Food & Wine** this place has been packed every night."')
+
 
