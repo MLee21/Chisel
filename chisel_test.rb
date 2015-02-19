d75df2f5e42c757259bf87f2d550ed674599a8bf
MLee21 <minnie.lee89@gmail.com>
2015-02-18 18:33:59 -0700

    added test for paragraph. simple construct passed and met output. working on refactoring / building.

--- chisel_test.rb
+++ chisel_test.rb
@@ -25,4 +25,17 @@ class ChiselTest < MiniTest::Test
 		assert_equal '##Chapter 1: The Beginning', chisel.header_2
 	end
 
+	def test_header_2_is_substituted
+		chisel = Chisel.new
+		document_part_two = '##Chapter 1: The Beginning'
+		assert_equal '<h2>Chapter 1: The Beginning</h2>',chisel.header_2
+	end
+
+	def test_paragraph_has_html_tags
+		chisel = Chisel.new
+		p2 = '"You just *have* to try the cheesecake," he said. "Ever since it appeared in
+ 		**Food & Wine** this place has been packed every night."'
+ 		assert_equal '<p>"You just *have* to try the cheesecake," he said. "Ever since it appeared in
+ 		**Food & Wine** this place has been packed every night."</p>', chisel.paragraph
+	end	
 end
