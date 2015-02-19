f47b833956f318a032f80b5b3bb7a7628d5883c0
MLee21 <minnie.lee89@gmail.com>
2015-02-18 22:15:42 -0700

    added asterisks method to ssub asterisks for em and strong. work in progress.

--- chisel.rb
+++ chisel.rb
@@ -1,24 +1,24 @@
 
 class Chisel
 
-	attr_reader :paragraph
+	attr_reader :paragraph, :document, :document_part_two
 	# 1. WE ARE SUBSTITUTING "#" WITH <h1> USING GSUB #
 	# 2. FIND A WAY TO RUN THROUGH STRING TO END AND ADD </h1>
 	def initialize
 		@paragraph = '"You just *have* to try the cheesecake," he said. "Ever since it appeared in
  		**Food & Wine** this place has been packed every night."'
+ 		@document = '#My Life in Desserts'
+ 		@document_part_two = '##Chapter 1: The Beginning'
 	end	
 
 
 	def header_1
-		document = '#My Life in Desserts'
-		substitute_h1 = document.gsub("#","<h1>")
+		substitute_h1 = @document.gsub("#","<h1>")
 		substitute_h1 + "</h1>"
 	end
 		
 	def header_2
-    	document_part_two = '##Chapter 1: The Beginning'
-    	substitute_h2 = document_part_two.gsub("##","<h2>")
+    	substitute_h2 = @document_part_two.gsub("##","<h2>")
     	substitute_h2 + "</h2>"
 	end
 
@@ -26,7 +26,20 @@ class Chisel
 		"<p>" + "#{@paragraph}" + "</p>"
 	end
 
-
+	#Run through sentence and find * encasing a word on each side 
+	#and **	encasing a word on each side
+	def asterisks
+		asterisks = @paragraph.split.map do |word| 
+			if word.include?("*")
+				word.gsub("*","<em>")
+			elsif word.include?("**")	
+				word.gsub("**","<strong>")
+			else	
+				word
+			end
+		end
+		asterisks.join(" ")
+	end
 
 # # parser = Chisel.new
 # # output = parser.parse(document)
@@ -34,12 +47,11 @@ class Chisel
 
 # #.each_line
 # #.gsub? and regex
+##.insert for h1, h2, and p
 end
 
 chisel = Chisel.new
-puts chisel.header_1
-puts chisel.header_2
-puts chisel.paragraph
+print chisel.asterisks
 
 # <p>
 #   "You just <em>have</em> to try the cheesecake," he said. "Ever since it appeared in
