f476e3fa27abd2fc2b4d381187d5d5b1596955a4
MLee21 <minnie.lee89@gmail.com>
2015-02-18 18:25:41 -0700

    added paragraph method. outcome expected. will start refactoring.

--- chisel.rb
+++ chisel.rb
@@ -1,8 +1,15 @@
 
 class Chisel
 
+	attr_reader :paragraph
 	# 1. WE ARE SUBSTITUTING "#" WITH <h1> USING GSUB #
 	# 2. FIND A WAY TO RUN THROUGH STRING TO END AND ADD </h1>
+	def initialize
+		@paragraph = '"You just *have* to try the cheesecake," he said. "Ever since it appeared in
+ 		**Food & Wine** this place has been packed every night."'
+	end	
+
+
 	def header_1
 		document = '#My Life in Desserts'
 		substitute_h1 = document.gsub("#","<h1>")
@@ -10,11 +17,16 @@ class Chisel
 	end
 		
 	def header_2
-    document_part_two = '##Chapter 1: The Beginning'
+    	document_part_two = '##Chapter 1: The Beginning'
+    	substitute_h2 = document_part_two.gsub("##","<h2>")
+    	substitute_h2 + "</h2>"
 	end
 
-# "You just *have* to try the cheesecake," he said. "Ever since it appeared in
-# **Food & Wine** this place has been packed every night."'
+	def paragraph
+		"<p>" + "#{@paragraph}" + "</p>"
+	end
+
+
 
 # # parser = Chisel.new
 # # output = parser.parse(document)
@@ -24,11 +36,10 @@ class Chisel
 # #.gsub? and regex
 end
 
-
-
-# <h1>My Life in Desserts</h1>
-
-# <h2>Chapter 1: The Beginning</h2>
+chisel = Chisel.new
+puts chisel.header_1
+puts chisel.header_2
+puts chisel.paragraph
 
 # <p>
 #   "You just <em>have</em> to try the cheesecake," he said. "Ever since it appeared in
