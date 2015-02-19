9320f01c63f317fd433247981ad651ca13081562
MLee21 <minnie.lee89@gmail.com>
2015-02-18 17:50:35 -0700

    added header 2 method.

--- chisel.rb
+++ chisel.rb
@@ -4,16 +4,14 @@ class Chisel
 	# 1. WE ARE SUBSTITUTING "#" WITH <h1> USING GSUB #
 	# 2. FIND A WAY TO RUN THROUGH STRING TO END AND ADD </h1>
 	def header_1
-		document = '# My Life in Desserts'
+		document = '#My Life in Desserts'
 		substitute_h1 = document.gsub("#","<h1>")
+		substitute_h1 + "</h1>"
 	end
-		# split_document = substitute_h1.split	
-		# print split_document
-# add_ending_h1 = substitute_h1.map.each_line {|string| 
-# 				if string.length 
-# }
-
-## Chapter 1: The Beginning
+		
+	def header_2
+    document_part_two = '##Chapter 1: The Beginning'
+	end
 
 # "You just *have* to try the cheesecake," he said. "Ever since it appeared in
 # **Food & Wine** this place has been packed every night."'
