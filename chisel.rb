b7ff5a16e1458f765addacb8898d5bb08fe615d2
MLee21 <minnie.lee89@gmail.com>
2015-02-18 17:39:50 -0700

    refined header 1 method to build other methods.

--- chisel.rb
+++ chisel.rb
@@ -3,29 +3,37 @@ class Chisel
 
 	# 1. WE ARE SUBSTITUTING "#" WITH <h1> USING GSUB #
 	# 2. FIND A WAY TO RUN THROUGH STRING TO END AND ADD </h1>
-# document = '# My Life in Desserts	
-substitute_h1 = document.gsub("#","<h1>")
+	def header_1
+		document = '# My Life in Desserts'
+		substitute_h1 = document.gsub("#","<h1>")
+	end
+		# split_document = substitute_h1.split	
+		# print split_document
+# add_ending_h1 = substitute_h1.map.each_line {|string| 
+# 				if string.length 
+# }
 
-# ## Chapter 1: The Beginning
+## Chapter 1: The Beginning
 
 # "You just *have* to try the cheesecake," he said. "Ever since it appeared in
 # **Food & Wine** this place has been packed every night."'
 
-# parser = Chisel.new
-# output = parser.parse(document)
-# # puts output
+# # parser = Chisel.new
+# # output = parser.parse(document)
+# # # puts output
 
-#.each_line
-#.gsub? and regex
+# #.each_line
+# #.gsub? and regex
 end
 
 
-<h1>My Life in Desserts</h1>
 
-<h2>Chapter 1: The Beginning</h2>
+# <h1>My Life in Desserts</h1>
 
-<p>
-  "You just <em>have</em> to try the cheesecake," he said. "Ever since it appeared in
-  <strong>Food &amp; Wine</strong> this place has been packed every night."
-</p>
+# <h2>Chapter 1: The Beginning</h2>
+
+# <p>
+#   "You just <em>have</em> to try the cheesecake," he said. "Ever since it appeared in
+#   <strong>Food &amp; Wine</strong> this place has been packed every night."
+# </p>
 
--- chisel_test.rb
+++ chisel_test.rb
@@ -13,4 +13,10 @@ class ChiselTest < MiniTest::Test
 	def test_chisel_exists
 		assert Chisel 
 	end	
+
+	def test_header_1_is_substituted
+		chisel = Chisel.new
+		document = '# My Life in Desserts'
+		assert_equal '<h1> My Life in Desserts', chisel.header_1
+	end	
 end
