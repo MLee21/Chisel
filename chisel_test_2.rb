d26d1e23bf92471b528a0efb024f838caa862fc0
MLee21 <minnie.lee89@gmail.com>
2015-02-21 11:53:15 -0700

    revised tests for new method in chisel_refactored.

--- chisel_test_2.rb
+++ chisel_test_2.rb
@@ -10,21 +10,34 @@ require_relative "chisel_refactored"
       assert true
     end
 
-    def test_method_that_identifies_h1_to_h6
+    def test_deletes_md_markers_adds_open_tag_and_closing_tag
+      chisel = Chisel.new
+      text = "#Test,test"
+      md_marker = "#"
+      open_tag = "<h1>"
+      close_tag = "</h1>"
+      assert_equal ("<h1>Test,test</h1>"), chisel.delete_and_sub_md_markers(text,md_marker,open_tag,close_tag)
+    end
+
+
+    def test_method_that_deletes_and_subs_h1_to_h6
         chisel = Chisel.new
-        text = "#Test,test"
+        text = "######Test,test"
         md_marker = "#"
-        assert_equal ("<h1>Test,test</h1>"), chisel.identify_md_marker(text,md_marker)
+        open_tag = "<h6>"
+        close_tag = "</h6>"
+        assert_equal ("<h6>Test,test</h6>"), chisel.delete_and_sub_md_markers(text,md_marker,open_tag,close_tag)
     end
 
 
-    def test_method_that_identifies_what_md_marker_equals_which_html_tag
+    def test_method_that_deletes_and_subs_paragraph_html
       skip
-    end
-
-    def test_method_that_will_appropriately_replace_md_marker_with_correct_html_tag
-      skip
-        chisel = Chisel.new
+      chisel = Chisel.new
+        text = "This is a test"
+        md_marker = ""
+        open_tag = "<p>"
+        close_tag = "</p>"
+        assert_equal ("<p>Test,test</p>"), chisel.delete_and_sub_md_markers(text,md_marker,open_tag,close_tag)
     end
 
     def test_it_can_correcly_replace_md_markers_in_a_simple_string
